ac0ce69329264ba42c655d6812d78e9200284e93
Your Name <you@example.com>
2019-11-02 02:52:23 -0300

    Integrate Socket IO

---
+++ src/_index.js
@@ -0,0 +1,52 @@
+/**
+ * Inicia variaveis de ambiente
+ */
+const dotenv = require('dotenv');
+dotenv.config();
+
+const five = require('johnny-five');
+const board = new five.Board({
+  port: process.env.PORT_COM
+});
+
+const PORT = process.env.PORT || 3333;
+const { getIP } = require('./services/ip');
+
+const express = require('express'),
+  app = express();
+
+/**
+ * @description Quando a placa estiver disponivel criara as rotas do servidor
+ */
+board.on('ready', () => {
+  let led = new five.Led(13);
+  let status = false;
+
+app.get('/acende', (req, res) => {
+  led.on();
+  status = true
+  return res.json({
+    status,
+    message: 'Acendeu'
+  });
+});
+
+app.get('/apaga', (req, res) => {
+  led.off();
+  status = false;
+  return res.json({
+    status,
+    message: 'Apagou'
+  });
+});
+
+app.get('/status', (req, res) => {
+  // led.off();
+  return res.json({
+    status,
+    message: status ? 'Acesa' : 'Apagado'
+  });
+});
+});
+
+app.listen(PORT, () => getIP(PORT));
--- src/index.js
+++ src/index.js
@@ -6,47 +6,49 @@ dotenv.config();
 
 const five = require('johnny-five');
 const board = new five.Board({
-  port: process.env.PORT_COM
+    port: process.env.PORT_COM
 });
 
-const PORT = process.env.PORT || 3333;
-const { getIP } = require('./services/ip');
 
-const express = require('express'),
-  app = express();
 
 /**
  * @description Quando a placa estiver disponivel criara as rotas do servidor
  */
 board.on('ready', () => {
-  let led = new five.Led(13);
-  let status = false;
+    let led = new five.Led(13);
+    let status = false;
 
-app.get('/acende', (req, res) => {
-  led.on();
-  status = true
-  return res.json({
-    status,
-    message: 'Acendeu'
-  });
+    const PORT = process.env.PORT || 3333;
+    const { getIP } = require('./services/ip');
+
+    const express = require('express'),
+        app = express();
+
+    const socketio = require('socket.io');
+
+    const server = require('http').createServer(app);
+    const io = socketio(server);
+
+    io.on('connection', socket => {
+        console.log(`Nova conexao: ${socket.id}`);
+        socket.broadcast.emit('status', status);
+        
+        socket.on('on', (data) => {
+            led.on();
+            status = true;
+            socket.broadcast.emit('status', status);
+            console.log(data)
+        })
+
+
+        socket.on('off', (data) => {
+            led.off();
+            status = false;
+            socket.broadcast.emit('status', status);
+            console.log(data)
+        })
+
+    })
+    server.listen(PORT, () => getIP(PORT))
 });
 
-app.get('/apaga', (req, res) => {
-  led.off();
-  status = false;
-  return res.json({
-    status,
-    message: 'Apagou'
-  });
-});
-
-app.get('/status', (req, res) => {
-  // led.off();
-  return res.json({
-    status,
-    message: status ? 'Acesa' : 'Apagado'
-  });
-});
-});
-
-app.listen(PORT, () => getIP(PORT));
